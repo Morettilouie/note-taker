868d2512f5b164458495caed8d9131089c97746c
Louie Moretti <morettilouie@gmail.com>
2022-10-13 08:42:51 -0500

    added post route to save notes and cleared db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const db = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,7 +19,21 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+});
+
+app.post('/api/notes', (req, res) => {
+    db.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(db), err => {
+        if (err) {
+            console.log(err)
+        }
+        res.json(db);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
